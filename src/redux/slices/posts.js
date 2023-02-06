83e539f341abce2446cf42eccdf1c132e036d3d7
texasjkee <gt.gluck@gmail.com>
2023-02-06 19:16:17 +0200

    Added createSlice and createAsyncThunk

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -1,1 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from '../../axios'
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const { data } = await axios.get('/posts');
+  return data;
+}); 
+
+
+const initialState = {
+  post: {
+    items: [],
+    status: 'loading' ,
+  },
+  tags: {
+    items: [],
+    status: "loading",
+  }
+}
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducer: {}, 
+})
+
+export const postsReducer = postsSlice.reducer;
